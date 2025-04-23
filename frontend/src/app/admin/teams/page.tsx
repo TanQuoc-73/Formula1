17843aeb9d0c9aad4d517cdc8126bd346d5f0a5e
TanQuoc-73 <cutorasusu2004@gmail.com>
2025-04-23 12:57:52 +0700

    'teamsadmin'

--- frontend/src/app/admin/teams/page.tsx
+++ frontend/src/app/admin/teams/page.tsx
@@ -2,7 +2,6 @@
 
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/24/solid';
 import NavBar from '@/components/NavBar';
 import Footer from '@/components/Footer';
 import Link from 'next/link';
@@ -13,7 +12,7 @@ export default function Admin() {
     return(
         <div className=''>
             <NavBar/>
-            <main className='h-screen w-screen flex'>
+            <main className='h-screen w-screen flex '>
                 <div className=''>
 
                 </div>
